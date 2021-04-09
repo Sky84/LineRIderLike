73352177a8b4f4fb8d8737d646ce963d2421b19d
Salupo Etienne <etienne.salupocorvo@gmail.com>
2021-04-09 19:58:23 +0200

    feature better colliding and lines

---
+++ Camera2DController.gd
@@ -0,0 +1,7 @@
+extends Camera2D
+
+onready var target: Node2D = $Ball;
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- LineController.gd
+++ LineController.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
 var mouse_left_down: bool = false;
-var radiusPoint = 4;
+var line_width = 4;
 
 var draws = {};
 var collisionsShapes = [];
+var is_next_point_first = false;
 onready var currentStaticBody: StaticBody2D = $TerrainStaticBody2D;
 
 func _input(event:InputEvent):
 	if event is InputEventMouseButton:
 		if event.button_index == 1 and event.is_pressed():
 			mouse_left_down = true;
+			is_next_point_first = true;
 		elif event.button_index == 1 and !event.is_pressed():
 			mouse_left_down = false;
 	elif Input.is_key_pressed(KEY_R):
@@ -30,17 +32,20 @@ func _update_collision(point1:Vector2, point2:Vector2):
 func _process(delta):
 	if mouse_left_down:
 		var current_point = {
-			"position": get_global_mouse_position(), 
-			"radius": radiusPoint,
-			"id": draws.size()
+			"position": get_global_mouse_position(),
+			"id": draws.size(),
+			"is_first_from_line":is_next_point_first,
 		};
+		is_next_point_first = false;
 		draws[current_point.id] = current_point;
-		if current_point.id - 1 in draws:
+		if current_point.id - 1 in draws and current_point.is_first_from_line == false:
 			var previous_point = draws[current_point.id - 1];
 			_update_collision(previous_point.position, current_point.position);
 		update();
 
 func _draw():
 	for key in draws:
-		var draw = draws[key];
-		draw_circle(draw.position, draw.radius, Color.black);
+		if key - 1 in draws and draws[key].is_first_from_line == false:
+			var current_point = draws[key];
+			var previous_point = draws[key-1];
+			draw_line(previous_point.position, current_point.position, Color.black, line_width);
