d9a8318e8d8228631a3cb047a8df9514ca4930b7
Salupo Etienne <etienne.salupocorvo@gmail.com>
2021-04-22 20:59:31 +0200

    level select WIP

--- Scripts/MainMenuController.gd
+++ Scripts/MainMenuController.gd
@@ -1,11 +1,14 @@
 extends Control
 
 onready var LevelsMenu = get_node("../LevelsMenu");
+onready var LevelsMenuBackground = get_node("../level_select");
 onready var MainMenu = get_node("../MainMenu");
+onready var MainMenuBackground = get_node("../main_menu");
 
 # Called when the node enters the scene tree for the first time.
 func _ready(): 
 	LevelsMenu.hide();
+	LevelsMenuBackground.hide();
 	MainMenu.show();
 
 
@@ -15,7 +18,9 @@ func _ready():
 
 func _on_PlayCampaignButton_button_up():
 	MainMenu.hide();
+	MainMenuBackground.hide();
 	LevelsMenu.show();
-	
+	LevelsMenuBackground.show();
+
 func _on_PlayInfiniteButton_button_up():
 	SceneManager.load_scene(SceneManager.Scene.InfiniteGame);
