5e5bad5c51b21f1fc9e17e247a53b08d575dc21e
Etienne Salupo <etienne.salupocorvo@gmail.com>
2021-04-23 13:10:27 +0200

    feat select level with animation

--- Objects/ButtonLevelController.gd
+++ Objects/ButtonLevelController.gd
@@ -2,12 +2,14 @@ extends Button
 
 onready var animationPlayer = $AnimationPlayer;
 
+var selected = false;
+signal on_selected_change(value);
+func _ready():
+	connect("on_selected_change", self, "on_select");
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_ButtonLevel_pressed():
-	animationPlayer.play("on_mouse_enter");
-	print("test")
+func on_select(value:bool):
+	if value and !selected:
+		animationPlayer.play("on_mouse_enter");
+	elif !value and selected:
+		animationPlayer.play_backwards("on_mouse_enter");
+	selected = value;
