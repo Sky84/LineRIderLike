0d289b429f00ff01e74029563533ce78b4d18890
Salupo Etienne <etienne.salupocorvo@gmail.com>
2021-05-08 13:24:32 +0200

    better background music system

--- Scripts/SceneManager.gd
+++ Scripts/SceneManager.gd
@@ -10,10 +10,10 @@ const Scene = {
 }
 
 const levels = [
-	{"index":1, "label":"Give", "path":"res://Scenes/Levels/Level_1.tscn", "path_preview":"res://Sprites/Previews/lvl1.png", "background_music":"res://Audio/PR1_export.wav"},
-	{"index":2, "label":"A", "path":"res://Scenes/Levels/Level_2.tscn", "path_preview":"res://Sprites/Previews/lvl2.png", "background_music":"res://Audio/PR1_export.wav"},
-	{"index":3, "label":"Second", "path":"res://Scenes/Levels/Level_3.tscn", "path_preview":"res://Sprites/Previews/lvl3.png", "background_music":"res://Audio/PR1_export.wav"},
-	{"index":4, "label":"Chance", "path":"res://Scenes/Levels/Level_4.tscn", "path_preview":"res://Sprites/Previews/lvl4.png", "background_music":"res://Audio/PR2_export.wav"},
+	{"index":1, "label":"Give", "path":"res://Scenes/Levels/Level_1.tscn", "path_preview":"res://Sprites/Previews/lvl1.png"},
+	{"index":2, "label":"A", "path":"res://Scenes/Levels/Level_2.tscn", "path_preview":"res://Sprites/Previews/lvl2.png"},
+	{"index":3, "label":"Second", "path":"res://Scenes/Levels/Level_3.tscn", "path_preview":"res://Sprites/Previews/lvl3.png"},
+	{"index":4, "label":"Chance", "path":"res://Scenes/Levels/Level_4.tscn", "path_preview":"res://Sprites/Previews/lvl4.png"},
 ];
 
 var currentScene;
@@ -26,6 +26,8 @@ signal on_scene_changed(path);
 func _ready():
 	load_scene(Scene.MainMenu);
 	connect("on_scene_changed", self, "_on_scene_changed");
+	AudioPlayer.stream = load("res://Audio/bg_music.mp3");
+	AudioPlayer.play();
 
 func load_scene(path:String):
 	if currentScene:
@@ -41,8 +43,6 @@ func load_level(level):
 	tween.start();
 	yield(tween, "tween_completed");
 	load_scene(level.path);
-	AudioPlayer.stream = load(level.background_music);
-	AudioPlayer.play();
 
 func _on_scene_changed(path):
 	tween.interpolate_property(transition, "modulate", Color(1,1,1,1), Color(1,1,1,0), 1, Tween.EASE_IN);
